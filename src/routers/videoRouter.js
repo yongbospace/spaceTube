76ad3edb6065d11e70d883f5d79f6266dee2e85c
yongboshim <94089402+yongbospace@users.noreply.github.com>
2021-12-07 15:58:18 +0900

    Route Complete

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,9 +9,9 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", edit);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/upload", Upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
