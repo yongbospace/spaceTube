6e1097b9366b095f63f225db613890e6bab806cd
yongboshim <94089402+yongbospace@users.noreply.github.com>
2021-12-09 16:09:56 +0900

    html pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,37 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const upload = (req, res) => res.send("upload", { pageTitle: "Upload" });
 export const deleteVideo = (req, res) => {
   return res.send("Delete Video");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,7 +9,7 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", Upload);
+videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", see);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
