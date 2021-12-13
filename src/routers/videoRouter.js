cd740f91c4fd1ce7c1ae8b777a3c044ebb10f067
yongboshim <94089402+yongbospace@users.noreply.github.com>
2021-12-13 15:25:15 +0900

    work with fake DB

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,17 +1,19 @@
 import express from "express";
 import {
-  see,
-  edit,
+  watch,
   upload,
   search,
   deleteVideo,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
