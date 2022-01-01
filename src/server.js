a17f6e209b78186592e7500e69c7998f6654680f
yongboshim <94089402+yongbospace@users.noreply.github.com>
2022-01-01 22:40:22 +0900

    mongoDB

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import globalRouter from "./routers/grobalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
 
@@ -17,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
