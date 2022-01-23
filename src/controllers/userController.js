9342a0284c30c7c83ecb3549f989c4bd3cac52e0
yongboshim <94089402+yongbospace@users.noreply.github.com>
2022-01-23 23:25:11 +0900

    edit profile templete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -132,9 +132,13 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
-export const edit = (req, res) => res.send("Edit User");
 export const logout = (req, res) => {
   req.session.destroy();
   res.redirect("/");
+};
+export const getEdit = (req, res) =>
+  res.render("edit-profile", { pageTitle: "Edit Profile" });
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
 };
 export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
-  edit,
   logout,
+  getEdit,
+  postEdit,
   see,
   startGithubLogin,
   finishGithubLogin,
@@ -10,7 +11,7 @@ import {
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
